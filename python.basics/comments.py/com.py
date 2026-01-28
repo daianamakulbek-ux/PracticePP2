fea61b02914dfd5a2e3d0dd10888ef0a555a9dce
daianamakulbek-ux <daianamakulbek@gmail.com>
2026-01-28 09:44:07 +0500

    com.py

--- python.basics/comments.py/com.py
+++ python.basics/comments.py/com.py
@@ -1,15 +1,10 @@
-# 1) Single-line comment
-x = 10  # 2) Inline comment
+# This is a comment
+print("Hello")  # Inline comment
 
-# 3) Multi-line comment style
-"""
-This is often used as a docstring or multi-line note.
-"""
+# Comment about variable
+x = 5
 
-# 4) Function docstring
-def greet(name):
-    """Return a greeting message."""
-    return f"Hi, {name}!"
-
-# 5) Commenting out code
-# print("This won't run")
+# Another comment
+print(x)
+""" long comment
+still"""
